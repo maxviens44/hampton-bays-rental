e6d0da27a14f0bd1e064215fdf31118aa7b73c52
maxviens44 <max.viens@gmail.com>
2025-10-21 10:10:16 -0400

    Refactor main.jsx to include routing and Helmet

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
+import Bookings from './pages/Bookings.jsx'
+import Reviews from './pages/Reviews.jsx'
+import USOpen2026 from './pages/USOpen2026.jsx'
+import Gallery from './pages/Gallery.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/bookings' element={<Bookings />} />
+          <Route path='/reviews' element={<Reviews />} />
+          <Route path='/us-open-2026' element={<USOpen2026 />} />
+          <Route path='/gallery' element={<Gallery />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+)
