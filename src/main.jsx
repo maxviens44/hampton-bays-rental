80d6dde6b6e1c2959202b604aff58e9775de046c
maxviens44 <max.viens@gmail.com>
2026-03-17 11:59:12 -0400

    Import React and createRoot for rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ViteReactSSG } from 'vite-react-ssg'
 
-export const createRoot = ViteReactSSG(<App />)
+createRoot(document.getElementById('root')).render(<App />)
